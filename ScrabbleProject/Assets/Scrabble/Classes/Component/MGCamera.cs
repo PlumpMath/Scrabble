d59328258ab1fa05603160df38c99ac73a6a80f6
aries sanchez sulit <aries.sulit@gmail.com>
2015-02-21 20:42:56 +0800

    Implemented dragging component.

---
+++ ScrabbleProject/Assets/Scrabble/Classes/Component/MGCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MGTools
+{
+	using Ext;
+
+	public class MGCamera : MonoBehaviour 
+	{
+		public static MGCamera Instance { get; private set; }
+
+		[SerializeField] private Camera m_scrabbleCamera;
+
+		private void Awake ()
+		{
+			this.Assert<Camera>(m_scrabbleCamera, "m_scrabbleCamera must not be null!");
+			MGCamera.Instance = this;
+		}
+
+		public Camera ScrabbleCamera
+		{
+			get { return m_scrabbleCamera; }
+		}
+	}
+}
---
+++ ScrabbleProject/Assets/Scrabble/Classes/Component/MGDraggable.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace MGTools
+{
+	using Ext;
+
+	[RequireComponent(typeof(BoxCollider2D))]
+	public class MGDraggable : MonoBehaviour 
+	{
+		private Vector3 m_screenPoint;
+		private Vector3 m_offset;
+		private Vector3 m_rackos;
+
+		private void OnMouseDown ()
+		{
+			//this.Log(Tags.Log, "Draggable::OnMouseDown pos:{0}", this.transform.localPosition);
+			m_rackos = this.transform.position;
+			m_screenPoint =  MGCamera.Instance.ScrabbleCamera.WorldToScreenPoint(m_rackos);
+			m_offset = m_rackos - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, m_screenPoint.z));
+		}
+
+		private void OnMouseDrag ()
+		{
+			Vector3 screenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, m_screenPoint.z);
+			Vector3 pos = Camera.main.ScreenToWorldPoint(screenPos);
+			this.transform.position = pos;
+		}
+
+		private void OnMouseUp ()
+		{
+			// TODO: Check for valid position
+			this.transform.position = m_rackos;
+		}
+	}
+}
