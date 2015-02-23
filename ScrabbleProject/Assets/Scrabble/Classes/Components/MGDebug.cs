4dbf72d488db25610c6e31d3a2531ef6ebbeb896
aries sanchez sulit <aries.sulit@gmail.com>
2015-02-24 03:11:09 +0800

    Added incomplete debug editor.

---
+++ ScrabbleProject/Assets/Scrabble/Classes/Components/MGDebug.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MGTools
+{
+	using Board;
+	using Events;
+	using Ext;
+
+	public class MGDebug : MonoBehaviour 
+	{
+		public static readonly int MAX_LETTERS = 7;
+
+		public static MGDebug Instance { get; private set; }
+
+		[SerializeField] private string m_debugLetters;
+		[SerializeField] private bool m_debugStartingWord;
+		[SerializeField] private bool m_debugGeneratedWord;
+
+		private void Awake ()
+		{
+			MGDebug.Instance = this;
+		}
+
+		public void ValidateLetters ()
+		{
+			m_debugLetters = Regex.Replace(m_debugLetters, @"[^a-zA-Z0-9 ]", "");
+
+			// trim the characters
+			if (m_debugLetters.Length > 7)
+			{
+				m_debugLetters = this.NormalizeLength(m_debugLetters, MAX_LETTERS);
+			}
+		}
+
+		private string NormalizeLength (string p_string, int p_len)
+		{
+			return p_string.Substring(0, p_len);
+		}
+
+		public bool IsStartingWord
+		{
+			get { return m_debugStartingWord; }
+			private set { m_debugStartingWord = value; }
+		}
+
+		public bool IsGeneratedWord
+		{
+			get { return m_debugGeneratedWord; }
+			private set { m_debugGeneratedWord = value; }
+		}
+	}
+}
---
+++ ScrabbleProject/Assets/Scrabble/Classes/Editor/MGDebugEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace MGEditor
+{
+	using MGTools;
+
+	[CustomEditor(typeof(MGDebug))]
+	public class MGDebugEditor : Editor 
+	{
+		public override void OnInspectorGUI ()
+		{
+			DrawDefaultInspector();
+			MGDebug editor = target as MGDebug;
+			
+			EditorGUILayout.BeginHorizontal();
+			{
+				if (DrawButton("Validate", "Validate the characters entered!", 40f))
+				{
+					editor.ValidateLetters();
+				}
+			}
+			EditorGUILayout.EndHorizontal();
+		}
+		
+		static bool DrawButton (string title, string tooltip, float width)
+		{
+			return GUILayout.Button(new GUIContent(title, tooltip));
+		}
+	}
+}
